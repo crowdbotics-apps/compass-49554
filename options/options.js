9104259c71c3758015784591e691df78122be52c
Crowdbotics <team@crowdbotics.com>
2024-10-02 08:50:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "compass-49554",
+  url: "https://compass-49554.botics.co",
+  api: "https://compass-49554.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
